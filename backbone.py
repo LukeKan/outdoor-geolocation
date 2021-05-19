c55a657d1748d7687c02dec7318a70b72cf67030
RukeK <luca.loria1997@gmail.com>
2021-05-19 19:25:51 +0200

    Import from dataset google-landmarks-v2

--- backbone.py
+++ backbone.py
@@ -1,23 +1,65 @@
+import os
+from glob import glob
+
 import tf as tf
+import pandas as pd
+import numpy as np
 import s2sphere
+from keras import layers
+from keras.applications.efficientnet import EfficientNetB0
 from tensorflow import losses
 from tensorflow.python.data import Dataset
 from tensorflow.python.keras.applications.resnet_v2 import ResNet101V2
 from tensorflow.python.keras.optimizer_v2.adam import Adam
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
-
+from keras.preprocessing import image
+from tqdm import tqdm
 
 class Backbone:
     targets = []
     data_gen = {}
     model = {}
-    scale = [224, 224]
-    train_dataset, valid_dataset = {}
+    scale = (224, 224)
+    sampling_rate = 0.02  # fraction of images from each landmark_id
+    random_state = 17  # for reproducibility
+    bs = 32
 
-    def __init__(self, classes, dataset_path, bs=1):
+    def __init__(self, classes, bs=32):
         self.targets = classes
-        train_gen, validation_gen = self._imgDataGen(dataset_dir=dataset_path, bs=bs)
-        self.train_dataset, self.valid_dataset = self._get_datasets(train_gen, validation_gen)
+        self.bs = bs
+        #train_gen, validation_gen = self._imgDataGen(dataset_dir=dataset_path, bs=bs)
+        #self.train_dataset, self.valid_dataset = self._get_datasets(train_gen, validation_gen)
+
+    def path_to_tensor(self, img_path):
+        # loads RGB image as PIL.Image.Image type
+        img = image.load_img(img_path, target_size=self.scale)
+
+        # convert PIL.Image.Image type to 3D tensor with shape (scale, 3)
+        x = image.img_to_array(img)
+
+        # convert 3D tensor to 4D tensor with shape (1, scale, 3) and return 4D tensor
+        return np.expand_dims(x, axis=0)
+
+    def paths_to_tensor(self, img_paths):
+        list_of_tensors = [self.path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+        return np.vstack(list_of_tensors)
+
+    def load_dataset(self, path, train_sample):
+        file_out = sorted(glob(path + '/*/*/*/*'))
+        file_out = np.array([s.replace("\\", "/") for s in file_out])
+
+        label_out = pd.Series(name="landmark_id")
+
+        for file in file_out:
+            filebase = os.path.basename(file)
+            name = os.path.splitext(filebase)[0]
+            temp = train_sample.landmark_id[train_sample["id"] == name]
+            label_out = label_out.append(temp)
+            print(name + " processed.")
+
+        label_out = np.array(pd.get_dummies(label_out))
+
+        return file_out, label_out
 
     def _imgDataGen(self, dataset_dir, bs):
         train_data_gen = ImageDataGenerator(rotation_range=40,
@@ -51,39 +93,33 @@ class Backbone:
                                                             color_mode='rgb')
         return train_gen, validation_gen
 
-    def _get_datasets(self, train_gen, validation_gen):
+    def get_dataset(self, train_df):
+        train_path = 'dataset/google-landmark-v2/data/train'
+        valid_path = 'dataset/google-landmark-v2/data/train'
+        #test_path = './test_images/'
+        train_file, train_target = self.load_dataset(train_path, train_df)
+        valid_file, valid_target = train_file, train_target
+        #test_file, test_target = self.load_dataset(test_path, train_sample)
+        train_tensors = self.paths_to_tensor(train_file).astype('float32') / 255
+        valid_tensors = train_tensors
+        #test_tensors = self.paths_to_tensor(test_file).astype('float32') / 255
+        return train_tensors, train_target, valid_tensors, valid_target
 
-        train_dataset = Dataset.from_generator(lambda: train_gen,
-                                                       output_types=(tf.float32, tf.float32),
-                                                       output_shapes=(
-                                                       [None, 224, 224, 3], [None, len(self.targets)]))
-        train_dataset = train_dataset.repeat()
-
-        # Validation
-        # ----------
-        valid_dataset = Dataset.from_generator(lambda: validation_gen,
-                                                       output_types=(tf.float32, tf.float32),
-                                                       output_shapes=(
-                                                       [None, 224, 224, 3], [None, len(self.targets)]))
-
-        # Repeat
-        valid_dataset = valid_dataset.repeat()
-        return train_dataset, valid_dataset
 
     def build(self):
-        self.model = ResNet101V2(
-            include_top=True, weights='imagenet', input_tensor=None,
-            input_shape=None, pooling=None, classes=len(self.targets)
-        )
+        input_shape = self.scale + (3,)
+        input_layer = layers.Input(shape=input_shape)
+        self.model = EfficientNetB0(include_top=False, weights='imagenet', classes=len(self.targets))(input_layer)
         for layer in self.model.layers:
             layer.trainable = False
 
         self.model.summary()
 
-    def compile(self, logdir='', optimizer=Adam(learning_rate=1e-3)):
+    def compile(self, optimizer=Adam(learning_rate=1e-3)):
         loss = losses.CategoricalCrossentropy()
         metrics = ['accuracy']
         self.model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
 
-    def train(self):
-        self.model.fit(x=self.train_dataset, epochs=60, steps_per_epoch=40, validation_data=self.valid_dataset, validation_steps=5)
+    def train(self, train_tensors, train_target, valid_tensors, valid_target):
+        self.model.fit(train_tensors, train_target, validation_data=(valid_tensors, valid_target), epochs=60,
+                       steps_per_epoch=40, verbose=1)
--- main.py
+++ main.py
@@ -1,55 +1,58 @@
 import os
 import matplotlib.pyplot as plt
 import cv2
-
-import s2sphere
 import pandas as pd
-bs = 64 # Batch size
-num_classes = 3
-class_list = ['Rock',
-              'Paper',
-              'Scissors']
-img_resize = [128, 128]
 
-def print_hi(name):
+from backbone import Backbone
+
+bs = 128  # Batch size
+
+num_classes = 203093
+class_list = range(0, 203093)
+img_resize = [256, 256]
+
+
+def get_paths(index_location):
+    index = os.listdir('dataset/google-landmark-v2/data/train/')
+    paths = []
+    a = index_location
+    for b in index:
+        for c in index:
+            try:
+                paths.extend([f"dataset/google-landmark-v2/data/train/{a}/{b}/{c}/" + x for x in
+                              os.listdir(f"dataset/google-landmark-v2/data/train/{a}/{b}/{c}")])
+            except:
+                pass
+
+    return paths
+
+
+def show_sample(pathes):
+    plt.rcParams["axes.grid"] = False
+    f, axarr = plt.subplots(3, 3, figsize=(20, 20))
+    axarr[0, 0].imshow(cv2.imread(pathes[0]))
+    axarr[0, 1].imshow(cv2.imread(pathes[1]))
+    axarr[0, 2].imshow(cv2.imread(pathes[2]))
+    axarr[1, 0].imshow(cv2.imread(pathes[3]))
+    axarr[1, 1].imshow(cv2.imread(pathes[4]))
+    axarr[1, 2].imshow(cv2.imread(pathes[5]))
+    axarr[2, 0].imshow(cv2.imread(pathes[6]))
+    axarr[2, 1].imshow(cv2.imread(pathes[7]))
+    axarr[2, 2].imshow(cv2.imread(pathes[8]))
+    plt.show()
+
+def main():
     # Use a breakpoint in the code line below to debug your script.
-    #model = Backbone()
-    train = pd.read_csv("dataset/google-landmark-v2/data/train.csv")
-
-    def get_paths(index_location):
-        index = os.listdir('dataset/google-landmark-v2/data/train/')
-        paths = []
-        a = index_location
-        for b in index:
-            for c in index:
-                try:
-                    paths.extend([f"dataset/google-landmark-v2/data/train/{a}/{b}/{c}/" + x for x in
-                                  os.listdir(f"dataset/google-landmark-v2/data/train/{a}/{b}/{c}")])
-                except:
-                    pass
-
-        return paths
-
-    def show_sample(pathes):
-        plt.rcParams["axes.grid"] = False
-        f, axarr = plt.subplots(3, 3, figsize=(20, 20))
-        axarr[0, 0].imshow(cv2.imread(pathes[0]))
-        axarr[0, 1].imshow(cv2.imread(pathes[1]))
-        axarr[0, 2].imshow(cv2.imread(pathes[2]))
-        axarr[1, 0].imshow(cv2.imread(pathes[3]))
-        axarr[1, 1].imshow(cv2.imread(pathes[4]))
-        axarr[1, 2].imshow(cv2.imread(pathes[5]))
-        axarr[2, 0].imshow(cv2.imread(pathes[6]))
-        axarr[2, 1].imshow(cv2.imread(pathes[7]))
-        axarr[2, 2].imshow(cv2.imread(pathes[8]))
-        plt.show()
+    model = Backbone(classes=class_list, bs=128)
+    train_df = pd.read_csv('dataset/google-landmark-v2/data/train.csv')
+    train_tensors, train_target, valid_tensors, valid_target = model.get_dataset(train_df)
+    model.build()
+    model.compile()
+    model.train(train_tensors, train_target, valid_tensors, valid_target)
 
     show_sample(get_paths(0))
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
